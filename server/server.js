742286263593e64676d351ff0763033bbea21cd7
Tbelzbby <belloafeez28@gmail.com>
2025-04-27 21:26:08 +0100

    Remove sensitive files from repository

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import connectDB from './config/db.js';
 import apiRoutes from './routes/api.js';
+import path from "path";
 
 dotenv.config();
 
@@ -13,8 +14,20 @@ connectDB();
 
 app.use(cors());
 app.use(express.json());
+
+// Serve static files from client/dist
+const __dirname = path.resolve();
+app.use(express.static(path.join(__dirname, '../client/dist')));
+
 app.use('/api', apiRoutes);
+
+// Serve frontend for all other routes (SPA)
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/dist', 'index.html'));
+});
 
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
 });
+
+export default app
